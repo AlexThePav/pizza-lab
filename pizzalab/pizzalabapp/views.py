096d0e461964b36947cfea2accef1ef5b9186ef4
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2021-01-22 17:50:34 +0200

    add views serializers
    added basic serializers, views, url routers

--- pizzalab/pizzalabapp/views.py
+++ pizzalab/pizzalabapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from pizzalabapp.models import Ingredient, Pizza, Order, OrderItem
+from pizzalabapp.serializers import (
+    IngredientSerializer, OrderItemSerializer, OrderSerializer, PizzaSerializer
+)
 
-# Create your views here.
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+class PizzaViewSet(viewsets.ModelViewSet):
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
