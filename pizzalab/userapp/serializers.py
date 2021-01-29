e1d3ea6473b34fdaa1883a39054ec5da3ee514eb
Teodora Budeanu <teodora.budeanu@softvision.com>
2021-01-29 14:12:35 +0200

    Add validation on PizzaSerializer and UserSerializer

--- pizzalab/userapp/serializers.py
+++ pizzalab/userapp/serializers.py
@@ -5,5 +5,8 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        # fields = ('username', 'email', 'password')
-        fields = '__all__'
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
