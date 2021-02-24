59fc0b4d649de5884250bd21aa2f681c41eff6e2
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2021-02-24 17:54:18 +0200

    cleaned up imports in views

--- pizzalab/pizzalabapp/views.py
+++ pizzalab/pizzalabapp/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status, viewsets
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.authentication import (
+    SessionAuthentication, TokenAuthentication
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
 
 from pizzalabapp.models import Ingredient, Order, OrderItem, Pizza
 from pizzalabapp.serializers import (IngredientSerializer, OrderItemSerializer,
